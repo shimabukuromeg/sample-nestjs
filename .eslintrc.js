daa38b0e9427a1f9d6817956a73f74d569cbe0bf
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-10-24 00:06:29 +0900

    リントの設定修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir : __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    // NOTE: 一旦外した
+    // 'plugin:prettier/recommended',
   ],
   root: true,
   env: {
