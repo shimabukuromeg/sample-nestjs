29a8f22ed0193fcb309713e4f0ef86c6a9a5f647
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-10-26 23:10:03 +0900

    Postを新規登録するmutationを作成した

--- src/components/posts/post.resolvers.ts
+++ src/components/posts/post.resolvers.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PostModel } from './interfaces/post.model';
 import { PrismaService } from '../../prisma.service'
+import { CreatePostInput } from './interfaces/create-post.input';
 
 @Resolver(() => PostModel)
 export class PostsResolver {
@@ -11,5 +12,14 @@ export class PostsResolver {
     @Query(() => [PostModel], { name: 'posts', nullable: true })
     async getPosts() {
         return this.prismaService.post.findMany();
+    }
+
+    @Mutation(() => PostModel)
+    async createPost(@Args('input') input: CreatePostInput) {
+        return this.prismaService.post.create({
+            data: {
+                title: input.title,
+            }
+        });
     }
 }
