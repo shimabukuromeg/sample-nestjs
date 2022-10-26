643b5fee7b121d60aa54b394585f91d62b80672a
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-10-26 22:58:28 +0900

    シンプルにしたかったので項目減らしました

--- prisma/migrations/20221026135635_init/migration.sql
+++ prisma/migrations/20221026135635_init/migration.sql
@@ -2,8 +2,6 @@
 CREATE TABLE "posts" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "published" BOOLEAN DEFAULT false,
-    "publish_date" TIMESTAMP(3),
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
