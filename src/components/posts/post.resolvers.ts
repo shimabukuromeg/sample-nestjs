83dff14fca6c5c95a9c235fecafc3b20c326bb7f
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-10-26 22:50:31 +0900

    とりあえず prisma と繋ぎ込んだ

--- src/components/posts/post.resolvers.ts
+++ src/components/posts/post.resolvers.ts
@@ -1,23 +1,15 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { PostModel } from './interfaces/post.model';
+import { PrismaService } from '../../prisma.service'
 
 @Resolver(() => PostModel)
 export class PostsResolver {
-    constructor() {
-        console.log("PostsResolver constructor");
+    constructor(private readonly prismaService: PrismaService) {
+        console.log("hello");
     }
 
     @Query(() => [PostModel], { name: 'posts', nullable: true })
     async getPosts() {
-        return [
-            {
-                id: '1',
-                title: 'こんにちは',
-            },
-            {
-                id: '2',
-                title: 'こんばんわ',
-            },
-        ];
+        return this.prismaService.post.findMany();
     }
 }
--- src/components/posts/posts.module.ts
+++ src/components/posts/posts.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostsResolver } from './post.resolvers';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-    providers: [PostsResolver],
+    providers: [PostsResolver, PrismaService],
 })
 export class PostsModule { }
