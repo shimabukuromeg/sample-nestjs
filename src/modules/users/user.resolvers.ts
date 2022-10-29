f227a9b4e5d729eb94b85e6129bc9b888bf4c75e
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-10-30 00:42:14 +0900

    userの権限に応じて叩けるqueryと叩けないqueryを分けた

--- src/modules/users/user.resolvers.ts
+++ src/modules/users/user.resolvers.ts
@@ -1,15 +1,31 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { UserModel } from './interfaces/user.model';
 import { PrismaService } from '../../prisma.service'
+import { LoggedInGuard } from '../../guard/logged-in.guard';
+import { AdminGuard } from '../../guard/admin.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver(() => UserModel)
 export class UsersResolver {
     constructor(private readonly prismaService: PrismaService) {
-        console.log("hello");
+        // console.log("hello");
     }
 
+    // NOTE:ログイン済みのユーザーのみが実行できる
+    @UseGuards(LoggedInGuard)
     @Query(() => [UserModel], { name: 'users', nullable: true })
     async getUsers() {
+        return this.prismaService.user.findMany({
+            where: {
+                isAdmin: false
+            }
+        });
+    }
+
+    // NOTE: Adminロールを持ったユーザーのみが実行できる
+    @UseGuards(AdminGuard)
+    @Query(() => [UserModel], { name: 'allUsers', nullable: true })
+    async getAllUsers() {
         return this.prismaService.user.findMany();
     }
 }
